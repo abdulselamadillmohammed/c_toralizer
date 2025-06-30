94838bcfe12621f9e1e87480c20dfe288dd9e4e4
Ashadowmonarch <forcoding39@gmail.com>
2025-06-30 13:34:30 -0400

    define structures

--- toralize.c
+++ toralize.c
@@ -10,5 +10,15 @@ function call | ip address | port number
 */
 
 int main(int argc, char *argv[]){
+    char *host;
+    int port;
+
+    if (argc < 3){
+        fprintf(stderr, "Usage: %s <host> <port>\n", argv[0]);
+        return -1;
+    }
+
+    host = argv[1];
+    port = argv[2];
 
 }
--- toralize.h
+++ toralize.h
@@ -1,4 +1,21 @@
 /* toralize.h */
 #include <stdio.h>
-#define PROXY "127.0.0.1"
-#define PROXYPORT 9050
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define PROXY       "127.0.0.1"
+#define PROXYPORT   9050
+
+typedef unsigned char int8;
+typedef unsigned short int int16;
+typedef unsigned int int32;
+
+/*
+
+		+----+----+----+----+----+----+----+----+----+----+....+----+
+		| VN | CD | DSTPORT |      DSTIP        | USERID       |NULL|
+		+----+----+----+----+----+----+----+----+----+----+....+----+
+    	   1    1      2              4           variable       1
+
+*/
